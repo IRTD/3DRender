9bb773d637da093422ec19ff67872b7ee5dac50b
IRTD <reisbaellchen17@gmail.com>
2024-04-16 11:01:31 +0200

    Matrix mul works

---
+++ cube/src/main.rs
@@ -0,0 +1,65 @@
+#![allow(unused)]
+
+use std::{rc::Rc, time::Duration};
+
+use sdl2::{event::Event, libc::sched_attr, pixels::Color, render::Canvas, video::Window};
+use trigger::matrix::Matrix4x4;
+
+fn main() {
+    let t1 = Matrix4x4::x_rot(0.05);
+    let t2 = Matrix4x4::y_rot(0.03);
+    let mut s1 = Matrix4x4::default();
+    s1[(0, 0)] = 150.0;
+    s1[(1, 1)] = 300.0;
+    let mut s2 = Matrix4x4::default();
+    s2[(0, 0)] = 230.0;
+    s2[(1, 1)] = 230.0;
+    sdl2_render("Tets", 800, 800, (s1, s2, t1, t2), |c, ctx| {
+        let v1: trigger::Vertex = ctx.0.clone().into();
+        let v2: trigger::Vertex = ctx.1.clone().into();
+        let p1 = (v1.x as i32, v1.y as i32);
+        let p2 = (v2.x as i32, v2.y as i32);
+        c.draw_line(p1, p2)?;
+        c.draw_line((p1.0, p2.1), (p2.0, p1.1))?;
+        ctx.0 = ctx.3 * (ctx.2 * ctx.0);
+        ctx.1 = ctx.2 * (ctx.3 * ctx.1);
+        std::thread::sleep(Duration::from_millis(15));
+        Ok(())
+    })
+    .unwrap();
+}
+
+fn sdl2_render<T, F>(
+    name: &str,
+    width: u32,
+    height: u32,
+    mut foreign_ctx: T,
+    f: F,
+) -> anyhow::Result<()>
+where
+    F: Fn(&mut Canvas<Window>, &mut T) -> Result<(), String>,
+{
+    let ctx = sdl2::init().unwrap();
+    let video_sub = ctx.video().unwrap();
+    let mut canvas = video_sub
+        .window(name, width, height)
+        .position_centered()
+        .build()?
+        .into_canvas()
+        .build()?;
+    let mut el = ctx.event_pump().unwrap();
+    'running: loop {
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+        canvas.set_draw_color(Color::WHITE);
+        f(&mut canvas, &mut foreign_ctx).unwrap();
+        canvas.present();
+        for event in el.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'running,
+                _ => {}
+            }
+        }
+    }
+    Ok(())
+}
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,107 @@
+use std::{
+    fmt::Display,
+    ops::{Index, IndexMut, Mul},
+};
+
+use crate::Vertex;
+
+#[derive(Default, Clone, Copy)]
 pub struct Matrix4x4 {
     m: [[f64; 4]; 4],
+}
+
+impl Matrix4x4 {
+    pub fn scale(factor: f64) -> Self {
+        let mut m = Matrix4x4::default();
+        for i in 0..=3 {
+            m[(i, i)] = factor;
+        }
+        m
+    }
+
+    pub fn x_rot(angle: f64) -> Self {
+        let mut m = Matrix4x4::default();
+        m[(0, 0)] = 1.0;
+        m[(1, 1)] = angle.cos();
+        m[(2, 2)] = angle.cos();
+        m[(2, 1)] = angle.sin();
+        m[(1, 2)] = -angle.sin();
+        m
+    }
+
+    pub fn y_rot(angle: f64) -> Self {
+        let mut m = Matrix4x4::default();
+        m[(0, 0)] = angle.cos();
+        m[(0, 2)] = angle.sin();
+        m[(1, 1)] = 1.0;
+        m[(2, 0)] = -angle.sin();
+        m[(2, 2)] = angle.cos();
+        m
+    }
+}
+
+impl From<[[f64; 4]; 4]> for Matrix4x4 {
+    fn from(m: [[f64; 4]; 4]) -> Self {
+        Matrix4x4 { m }
+    }
+}
+
+impl From<Vertex> for Matrix4x4 {
+    fn from(v: Vertex) -> Self {
+        let mut m = Matrix4x4::default();
+        m[(0, 0)] = v.x;
+        m[(1, 1)] = v.y;
+        m[(2, 2)] = v.z;
+        m
+    }
+}
+
+impl Into<Vertex> for Matrix4x4 {
+    fn into(self) -> Vertex {
+        Vertex::from([self[(0, 0)], self[(1, 1)], self[(2, 2)]])
+    }
+}
+
+impl Mul for Matrix4x4 {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut new = Matrix4x4::default();
+        for i in 0..=3 {
+            for j in 0..=3 {
+                for k in 0..=3 {
+                    new[(k, i)] += self[(i, j)] * rhs[(k, j)];
+                }
+            }
+        }
+        new
+    }
+}
+
+impl Display for Matrix4x4 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut s = String::from("   0.\t1.\t2.\t3.\n");
+        for i in 0..=3 {
+            let col = format!("{i}. ");
+            s.push_str(&col);
+            for j in 0..=3 {
+                let p = format!("{}\t", self[(i, j)]);
+                s.push_str(&p);
+            }
+            s.push('\n');
+        }
+        f.write_str(&s)
+    }
+}
+
+impl Index<(usize, usize)> for Matrix4x4 {
+    type Output = f64;
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        &self.m[index.0][index.1]
+    }
+}
+
+impl IndexMut<(usize, usize)> for Matrix4x4 {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        &mut self.m[index.0][index.1]
+    }
 }
